5db7f6ec965bc4ffc51d0be1fa2a95522e259cf4
ParkJoohyun <terajoohyun@ajou.ac.kr>
2021-04-30 16:19:13 +0900

    타겟넘버 코드 업데이트

--- programmers/DFS&BFS/타겟넘버/타겟넘버.cpp
+++ programmers/DFS&BFS/타겟넘버/타겟넘버.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <queue>
+#include <tuple>
 
 using namespace std;
 int _count = 0;
@@ -19,12 +21,43 @@ void dfs(vector<int> check, vector<int> num, int target, int current, int idx, i
         }
     }
 }
+
+void bfs(vector<int> check, vector<int> num, int target) {
+    queue<tuple<int, int, int>> q; // idx, neg
+    q.push(make_tuple(0, 1, num[0]*1));
+    q.push(make_tuple(0, -1, num[0] * (-1)));
+    check[0] = 1;
+
+    int idx, neg;
+    int sum = 0;
+
+
+    while (!q.empty()) {
+        
+        idx = get<0>(q.front());
+        neg = get<1>(q.front());
+        sum = get<2>(q.front());
+        q.pop();
+
+        if (target == sum && idx == num.size() - 1) {
+            _count += 1;
+            continue;
+        }
+        else if (idx >= num.size() -1) {
+            continue;
+        }
+        else {
+            q.push(make_tuple(idx + 1, 1, sum + num[idx + 1] * 1));
+            q.push(make_tuple(idx + 1, -1, sum + num[idx + 1] * (-1)));
+            continue;
+        }
+    }
+}
 int solution(vector<int> numbers, int target) {
     int answer = 0;
     vector<int> check(numbers.size(), 0);
-    // dfs 정석 대로
-    dfs(check, numbers, target, 0, 0, 0);
-
+    // dfs(check, numbers, target, 0, 0, 0);
+    bfs(check, numbers, target);
     cout << _count << endl;
     return _count;
 }
