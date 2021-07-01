0641d94c4f288e87621eaf1980f704994ef2c149
ParkJoohyun <terajoohyun@ajou.ac.kr>
2021-07-01 17:10:58 +0900

    feat: baekjoon 13300

--- programmers/Greedy/큰수만들기/큰수만들기.cpp
+++ programmers/Greedy/큰수만들기/큰수만들기.cpp
@@ -1,27 +1,24 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <cmath>
 
 using namespace std;
 
-
 string solution(string number, int k) {
     string answer = "";
-    char _max = '0';
-    int idx = -1;
-    int numsize = number.length() - k;
+    int numSize = number.size() - k;
+    int maxIdx = -1;
+    char maxNum;
     
-    for(int i = 0 ; i < numsize ; i++){
-        _max = '0';
-        for(int j = idx + 1 ; j < i + (k + 1) ; j++){
-            if(_max < number.at(j)){
-                _max = number[j];
-                idx = j;
+    for(int i=0 ; i<numSize ; i++) {
+        maxNum = '0';
+        for(int j = maxIdx + 1; j < i + k + 1 ; j++) {
+            // i + k + 1 까지의 index 중에서 최대값 찾아야 한다.
+            if(maxNum < number.at(j)) {
+                maxNum = number.at(j);
+                maxIdx = j;
             }
         }
-        answer += _max;
+        answer += maxNum;
     }
-    
     return answer;
 }
