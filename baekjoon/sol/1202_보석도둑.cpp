a07d98c1df01749f579eb6aaff9006401acefb1e
ParkJuHyeon <terajoohyun@ajou.ac.kr>
2021-07-08 17:56:55 +0900

    feat: baekjoon 1202 보석도둑
    min heap 을 사용한 약간의 그리디한 문제이다.
    작은 가방부터 순회하면서 가성비 좋은 보석을 넣으면 된다.

---
+++ baekjoon/sol/1202_보석도둑.cpp
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+typedef long long LL;
+typedef pair<LL, LL> pii;
+int n, k;
+
+int main()
+{
+    scanf("%d %d", &n, &k);
+    vector<pii> jewl;
+    vector<LL> bag;
+    priority_queue<LL> pq;
+
+    LL m, v, t;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> m >> v;
+        jewl.push_back({m, v});
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        cin >> t;
+        bag.push_back(t);
+    }
+    sort(bag.begin(), bag.end());
+    sort(jewl.begin(), jewl.end());
+
+    long long answer = 0;
+    int idx = 0;
+    for (int i = 0; i < k; i++)
+    {
+        while (idx < n && jewl[idx].first <= bag[i])
+        {
+            // 2. 가방에 넣을 수 있는지, 없는지 확인
+            pq.push(jewl[idx++].second);
+        }
+        if (!pq.empty())
+        {
+            answer += pq.top();
+            pq.pop();
+        }
+    }
+
+    // 1. 가장 작은 가방 기준
+    cout << answer;
+
+    return 0;
+}
