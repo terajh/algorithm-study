564961bad4c0e94b1d31adf32a7c7f3faf1f171d
ParkJuHyeon <terajoohyun@ajou.ac.kr>
2021-07-08 17:54:07 +0900

    feat: backjoon 14476 최대공약수 하나 빼기
    memo 배열을 앞, 뒤 방향으로 2개를 두어 하나 뺏을 때의 최대 공약수를 바로 구하게 끔 구현하면 됨

---
+++ baekjoon/sol/14476_최대공약수하나뺴기.cpp
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+typedef long long LL;
+LL arr[1 << 20];
+LL up_memo[1 << 20];
+LL down_memo[1 << 20];
+
+LL gcd(LL a, LL b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+int main()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    LL answer = 0;
+    LL g;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1)
+            g = arr[i];
+        else
+            g = gcd(g, arr[i]);
+        up_memo[i] = g; // i 까지의 up gcd
+    }
+
+    for (int i = n; i > 0; i--)
+    {
+        if (i == n)
+            g = arr[i];
+        else
+            g = gcd(g, arr[i]);
+        down_memo[i] = g; // i 까지의 down gcd
+    }
+
+    int answer_g = 0;
+    LL tp_gcd;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == 1)
+        {
+            tp_gcd = down_memo[2];
+        }
+        else if (i == n)
+        {
+            tp_gcd = up_memo[n - 1];
+        }
+        else
+        {
+            tp_gcd = gcd(down_memo[i + 1], up_memo[i - 1]);
+        }
+        if (answer < tp_gcd)
+        {
+            answer = tp_gcd;
+            answer_g = i;
+        }
+    }
+
+    if (arr[answer_g] % answer == 0)
+    {
+        cout << -1;
+    }
+    else
+    {
+        cout << answer << " " << arr[answer_g];
+    }
+}
