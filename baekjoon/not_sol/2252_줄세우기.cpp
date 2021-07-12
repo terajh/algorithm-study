c76514e19232263271c42f38f82541b0f1d5e95c
ParkJuHyeon <terajoohyun@ajou.ac.kr>
2021-07-12 17:58:48 +0900

    feat: baekjoon 2252 줄세우기
    배열과 queue 를 사용한 위상정렬 문제이다.

---
+++ baekjoon/not_sol/2252_줄세우기.cpp
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <queue>
+#include <iostream>
+#include <math.h>
+#include <map>
+#include <string.h>
+
+using namespace std;
+
+vector<int> v[32001];
+int indegree[32001];
+
+int main()
+{
+    queue<int> q;
+    int n, m, a, b, i;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        scanf("%d %d", &a, &b);
+        indegree[b] += 1;
+        v[a].push_back(b);
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if (indegree[i] == 0)
+        {
+            q.push(i); // 처음 0만 들어간다.
+        }
+    }
+
+    while (!q.empty())
+    {
+        i = q.front();
+        q.pop();
+
+        printf("%d ", i);
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            indegree[v[i][j]] -= 1;
+            if (indegree[v[i][j]] == 0)
+                q.push(v[i][j]);
+        }
+    }
+    printf("\n");
+    return 0;
+}
