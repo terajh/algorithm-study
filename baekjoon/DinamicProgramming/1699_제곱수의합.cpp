6276e227bf8aada1ebb579068bd09a274fd9d89b
terajh <terajoohyun@ajou.ac.kr>
2021-10-29 19:07:53 +0900

    feat: baekjoon 1699 제곱수의합 (dp)

---
+++ baekjoon/DinamicProgramming/1699_제곱수의합.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+vector<int> num;
+int dp[100001];
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i=1 ; i<=n ; i++) {
+        dp[i] = i;
+    }
+
+    for(int i=1 ; i<=n ; i++) {
+        for(int j=1 ; j*j<=i ; j++) {
+            dp[i] = min(dp[i], dp[i - j*j] + 1);
+        }
+    }
+
+    cout << dp[n];
+
+
+}
