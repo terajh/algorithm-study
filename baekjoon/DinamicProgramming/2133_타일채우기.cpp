2e0356805f0bb92beb2d42093d32e4146e6c7875
terajh <terajoohyun@ajou.ac.kr>
2021-11-06 18:55:34 +0900

    feat: baekjoon 2133 타일채우기

--- baekjoon/DinamicProgramming/2133_타일채우기.cpp
+++ baekjoon/DinamicProgramming/2133_타일채우기.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 
 using namespace std;
 
-int dp[31];
+int dp[100];
 int main() {
-    int n;
-    cin >> n;
-    
-    dp[1] = 0;
-    dp[2] = 3;
-
-    for(int i=3 ; i<=n ; i++) {
-        if(dp[i-2]!=0) dp[i] = dp[i-2] + 3;
-    }
-    cout << dp[n];
+	int n;
+	cin >> n;
+	// 2의 배수는 2개씩 추가된다.
+	dp[0] = 0;
+	dp[1] = 0;
+	dp[2] = 3;
+	
+	for(int i=4; i<=n ; i+=2) {
+		for(int j=2 ; j<=i ; j+=2) {
+			if(j == 2) 
+				dp[i] += dp[i-j] * 3;
+			else if(j != 2)
+				dp[i] += dp[i-j] * 2;
+		}
+		dp[i] += 2;
+	}
+	cout << dp[n];
 }
